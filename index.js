a9e622450a2807b24736eef32333d1db4f8b6c34
Kashish Gupta <kashishgupta99017@gmail.com>
2025-03-24 16:46:35 +0530

    update code showing with day and tme

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 document.getElementById("locationSelect").addEventListener("change", function() {
     let selectedTimezone = this.value;
 
-    if (selectedTimezone) {
+    if (selectedTimezone === "current") {
+        getCurrentLocationTime();
+    } else {
         updateTime(selectedTimezone);
         setInterval(() => updateTime(selectedTimezone), 1000);
-    } else {
-        document.getElementById("selectedLocation").innerText = "Select a location";
-        document.getElementById("currentTime").innerText = "--:--:--";
-        document.getElementById("currentDate").innerText = "";
     }
 });
 
@@ -22,4 +20,28 @@ function updateTime(timezone) {
 
     let locationText = document.getElementById("locationSelect").selectedOptions[0].text;
     document.getElementById("selectedLocation").innerText = locationText;
+}
+
+function getCurrentLocationTime() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+
+            fetch(`https://api.timezonedb.com/v2.1/get-time-zone?key=YOUR_API_KEY&format=json&by=position&lat=${lat}&lng=${lon}`)
+                .then(response => response.json())
+                .then(data => {
+                    let timezone = data.zoneName;
+                    updateTime(timezone);
+                })
+                .catch(error => {
+                    console.error("Error fetching time zone:", error);
+                    document.getElementById("selectedLocation").innerText = "Error getting location";
+                });
+        }, () => {
+            document.getElementById("selectedLocation").innerText = "Location access denied";
+        });
+    } else {
+        document.getElementById("selectedLocation").innerText = "Geolocation not supported";
+    }
 }
